47e5a504ffc70fd82eb7eef7121fed70198d4f38
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-01 12:31:41 +0100

    Update MovieCard with prop onMovieClick
    Extract the onMovieClick prop passed from MainView parent component. Call the passed function within the callback of the onClick event listener.

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -1,6 +1,15 @@
 //Create MovieCard child component
-//Pass data from parent component (MainView) to the child component (MovieCard) by using props
+//Pass data and function from parent component (MainView) to the child component (MovieCard) by using props.
 //Export the created child component MovieCard, and return the title of the movies
-export const MovieCard = ({ movie }) => {
-    return <div>{movie.title}</div>;
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (
+        <div
+            // After extracting the onMovieClick prop, call the passed function within the callback of the onClick event listener.
+            onClick={() => {
+                onMovieClick(movie);
+            }}
+        >
+            {movie.title}
+        </div>
+    );
 };

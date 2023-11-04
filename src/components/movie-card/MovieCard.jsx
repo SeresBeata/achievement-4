54f116f580ace41b84f69866b1e842492bacd27c
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-04 12:06:45 +0100

    Define props constraints at MovieCard
    Import PropTypes and define all the props constraints for the MovieCard.

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -1,3 +1,6 @@
+// Import PropTypes
+import PropTypes from 'prop-types';
+
 //Create MovieCard child component
 //Pass data and function from parent component (MainView) to the child component (MovieCard) by using props.
 //Export the created child component MovieCard, and return the title of the movies
@@ -12,4 +15,12 @@ export const MovieCard = ({ movie, onMovieClick }) => {
             {movie.title}
         </div>
     );
+};
+
+//Define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired,
 };

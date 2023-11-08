d8b2dc2869ca1b18ee6dacd4ffcdb1a0b819e83a
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-08 10:52:04 +0100

    Relocate state variables in MainView
    Relocate state variables called movies and selectedMovie in MainView.jsx file.

--- src/components/main-view/MainView.jsx
+++ src/components/main-view/MainView.jsx
@@ -6,14 +6,14 @@ import { LoginView } from '../login-view/LoginView';
 
 //Export the created MainView component
 export const MainView = () => {
-    //Use "useState" to declare a "state variable", called movies. Pass the initial state (which is an empty array) as an argument to the useState().
-    const [movies, setMovies] = useState([]);
-    //Create state variable, called selectedMovie, where the initial value of selectedMovie state is null.
-    const [selectedMovie, setSelectedMovie] = useState(null);
     //Create state variable, called user with initial stale "null". Use to check if user is logged in or not.
     const [user, setUser] = useState(null);
     //Create state variable, called token with initial state "null". Use to store token.
     const [token, setToken] = useState(null);
+    //Use "useState" to declare a "state variable", called movies. Pass the initial state (which is an empty array) as an argument to the useState().
+    const [movies, setMovies] = useState([]);
+    //Create state variable, called selectedMovie, where the initial value of selectedMovie state is null.
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
     //Fetch data from API and populate movies state using setMovies, with the fetched movies array from myFlix API
     useEffect(() => {

557a0a2bceb7a99ec8892e01727d957e4c24401f
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-11 16:53:36 +0100

    Update MovieCard with React-Bootstrap
    Import components from React-Bootstrap. Use the imported components to create <Card> components, with their sub-components.

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -1,11 +1,30 @@
 // Import PropTypes
 import PropTypes from 'prop-types';
+//Import components from React Bootstrap
+import { Button, Card } from 'react-bootstrap';
 
 //Create MovieCard child component
 //Pass data and function from parent component (MainView) to the child component (MovieCard) by using props.
 //Export the created child component MovieCard, and return the title of the movies
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
+        <Card
+            className="h-100 movie-card--bg"
+            // After extracting the onMovieClick prop, call the passed function within the callback of the onClick event listener.
+            onClick={() => onMovieClick(movie)}
+        >
+            <Card.Img variant="top" src={movie.imagePath} />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Button
+                    // After extracting the onMovieClick prop, call the passed function within the callback of the onClick event listener.
+                    onClick={() => onMovieClick(movie)}
+                    variant="link"
+                >
+                    Open
+                </Button>
+            </Card.Body>
+        </Card>
     );
 };
 

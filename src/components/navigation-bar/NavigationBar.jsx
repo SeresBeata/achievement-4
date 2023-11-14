6df77574a50fa470989198c43373ba207b79327c
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-14 18:33:06 +0100

    Create NavigationBar Child component
    Create navigation-bar folder and inside of it, create NavigationBar.jsx file. Import <Navbar> and sub-components from react-bootstrap and import <Link> from react-router-dom. Create NavigationBar child component and export it.  Display the login and signup links if user is logged out, and display the home and logout links, if user is logged in. Use the onLoggedOut prop to be called when logout is clicked.

---
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -0,0 +1,49 @@
+//Import from React-Bootstrap
+import { Navbar, Container, Nav } from 'react-bootstrap';
+//Import from react-router-dom
+import { Link } from 'react-router-dom';
+
+//Create NavigationBar child component and export it
+export const NavigationBar = ({ user, onLoggedOut }) => {
+    return (
+        <Navbar expand="lg" className="navbar-bg navbar-dark">
+            <Container>
+                <Navbar.Brand
+                    as={Link}
+                    to="/"
+                    style={{ color: '#fbd6da', fontWeight: 'bolder' }}
+                >
+                    myFlix
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {/* If user is "falsy", then display the login and signup links */}
+                        {!user && (
+                            <>
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/signup">
+                                    Signup
+                                </Nav.Link>
+                            </>
+                        )}
+                        {/* If user is "truthy", then display the home and logout links */}
+                        {user && (
+                            <>
+                                <Nav.Link as={Link} to="/">
+                                    Home
+                                </Nav.Link>
+                                {/* Use the onLoggedOut prop to be called when logout is clicked. */}
+                                <Nav.Link onClick={onLoggedOut}>
+                                    Logout
+                                </Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};

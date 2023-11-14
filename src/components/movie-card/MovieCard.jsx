3c81054e22cce8a48a5710176dd2a2d1167f7607
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-14 18:13:27 +0100

    Refactor MovieCard for state-based router
    Import <Link> component from react-router-dom. Delete the onMovieClick prop. Use the <Link> component to redirect the user to the selected movie. Update "MovieCard.propTypes ".

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -2,27 +2,25 @@
 import PropTypes from 'prop-types';
 //Import components from React Bootstrap
 import { Button, Card } from 'react-bootstrap';
+//Import from react-router-dom
+import { Link } from 'react-router-dom';
 
 //Create MovieCard child component
 //Pass data and function from parent component (MainView) to the child component (MovieCard) by using props.
 //Export the created child component MovieCard, and return the title of the movies
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
     return (
-        <Card
-            className="h-100 movie-card--bg"
-            // After extracting the onMovieClick prop, call the passed function within the callback of the onClick event listener.
-            onClick={() => onMovieClick(movie)}
-        >
-            <Card.Img variant="top" src={movie.imagePath} />
+        <Card className="h-100 movie-card--bg">
+            {/* Redirect the user to the selected movie with <Link>. */}
+            <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                <Card.Img variant="top" src={movie.imagePath} />
+            </Link>
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
-                <Button
-                    // After extracting the onMovieClick prop, call the passed function within the callback of the onClick event listener.
-                    onClick={() => onMovieClick(movie)}
-                    variant="link"
-                >
-                    Open
-                </Button>
+                {/* Redirect the user to the selected movie with <Link>. */}
+                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                    <Button variant="link">Open</Button>
+                </Link>
             </Card.Body>
         </Card>
     );
@@ -33,5 +31,4 @@ MovieCard.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired,
 };

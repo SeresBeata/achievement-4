2773a510cd4a3672addc9e29b5c2ac6a551860e4
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-15 09:33:02 +0100

    Create ProfileView child component
    Create profile-view folder and inside of it, create ProfileView child component and export it. Import components from React-Bootstrap. Use the "user" prop to return information about the current user. Use react-bootstrap components for styling.

---
+++ src/components/profile-view/ProfileView.jsx
@@ -0,0 +1,66 @@
+//Import components from React Bootstrap
+import { Card, ListGroup, Row, Col } from 'react-bootstrap';
+
+//Create ProfileView child component and export it
+export const ProfileView = ({ user }) => {
+    return (
+        <Row className="justify-content-md-center justify-content-sm-center">
+            <Col
+                lg={6}
+                md={12}
+                sm={12}
+                xs={12}
+                className="justify-content-md-center justify-content-sm-center"
+            >
+                <Card
+                    style={{ textAlign: 'center', margin: '30px 20px 0 20px' }}
+                >
+                    <Card.Header
+                        style={{
+                            backgroundColor: '#bf6466',
+                            color: '#ede0df',
+                            fontWeight: 'bolder',
+                        }}
+                    >
+                        Your Profile
+                    </Card.Header>
+                    <ListGroup>
+                        <ListGroup.Item>
+                            <span
+                                style={{
+                                    color: '#2c2425',
+                                    fontWeight: 'bolder',
+                                }}
+                            >
+                                Username:
+                            </span>{' '}
+                            {user.username}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <span
+                                style={{
+                                    color: '#2c2425',
+                                    fontWeight: 'bolder',
+                                }}
+                            >
+                                Email:
+                            </span>{' '}
+                            {user.email}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <span
+                                style={{
+                                    color: '#2c2425',
+                                    fontWeight: 'bolder',
+                                }}
+                            >
+                                Birthday:
+                            </span>{' '}
+                            {user.birthday.split('T')[0]}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+    );
+};

241d33c2a813daa40fe2ba73beed9d510a5443e3
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-16 09:11:53 +0100

    Update FavMovieView in case of no fav movies
    Import <Card> component from react-bootstrap. Use ternary operator to return message inside of a the imported <Card> component in case of no favorite movies, or return the favorite movies as <MovieCard>, if they exist.

--- src/components/favMovie-view/FavMovieView.jsx
+++ src/components/favMovie-view/FavMovieView.jsx
@@ -1,5 +1,5 @@
 //Import components from React Bootstrap
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import { MovieCard } from '../movie-card/MovieCard';
 
 //Create FavMovieView child component and export it
@@ -16,19 +16,35 @@ export const FavMovieView = ({ user, movies }) => {
     });
 
     return (
-        <Row className="justify-content-md-center justify-content-sm-center">
-            {favoriteMovies.map((movie) => (
-                <Col
-                    key={movie._id}
-                    className="mb-4"
-                    lg={3}
-                    md={4}
-                    sm={6}
-                    xs={12}
-                >
-                    <MovieCard movie={movie} />
+        <Row className="justify-content-md-center justify-content-sm-center ">
+            {/* Use ternary operator to check if there are favourite movies and return message, if there is no fav movie. */}
+            {favoriteMovies.length === 0 ? (
+                <Col lg={6} md={8} sm={10}>
+                    <Card
+                        style={{
+                            textAlign: 'center',
+                            fontWeight: 'bolder',
+                            margin: '50px 15px  0 15px',
+                        }}
+                    >
+                        <Card.Body> There is no Favourite Movie yet!</Card.Body>
+                    </Card>
                 </Col>
-            ))}
+            ) : (
+                //If there are fav movies, then return them as a <MovieCard>
+                favoriteMovies.map((movie) => (
+                    <Col
+                        key={movie._id}
+                        className="mb-4"
+                        lg={3}
+                        md={4}
+                        sm={6}
+                        xs={12}
+                    >
+                        <MovieCard movie={movie} />
+                    </Col>
+                ))
+            )}
         </Row>
     );
 };

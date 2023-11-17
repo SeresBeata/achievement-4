30b48a0695cc0bea17029345b7f992ad9ceb272e
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-17 17:45:04 +0100

    Refactor MovieCard to add/remove movie to fav-list
    Refactor MovieCard.jsx file to add/remove movie to fav-movie-list. Add token and setUser props. Update isFavourite state var with "fav-movie-list" initial state. Delete unnecessary useEffect(). Use fetch() to add/remove movie to fav-movie-list. Delete <i> elements. Use react-bootstrap <Button> and .svg file to create btn for fav-movie-list. Use ternary operator to check if the movie is one of the favourite movies, if yes, then return "full-red-heart". Use addFavoriteMovie to add the movie, and use removeFavoriteMovie to remove the movie on click event.

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -14,18 +14,67 @@ import { useEffect } from 'react';
 //Create MovieCard child component
 //Pass data and function from parent component (MainView) to the child component (MovieCard) by using props.
 //Export the created child component MovieCard, and return the title of the movies
-export const MovieCard = ({ user, movie }) => {
-    // Create state variable, called isFavorite, where the initial state is "false"
-    const [isFavorite, setIsFavorite] = useState(false);
+export const MovieCard = ({ user, movie, token, setUser }) => {
+    // Create state variable, called isFavorite, where the initial state is "fav-movie-list"
+    const [isFavorite, setIsFavorite] = useState(
+        JSON.stringify(user.favouriteMovies).includes(movie._id)
+    );
 
-    //Set the isFavorite state variable to "true", if there is fav movie of the current user
-    useEffect(() => {
-        //console.log(user.favouriteMovies);
-        if (user.favouriteMovies && user.favouriteMovies.includes(movie._id)) {
-            setIsFavorite(true);
-        }
-    }, [user]);
-    console.log(isFavorite);
+    console.log(
+        JSON.stringify(user.favouriteMovies) + '\n\n' + JSON.stringify(movie)
+    );
+
+    //Use fetch() to add movie to fav-movie-list
+    const addFavoriteMovie = () => {
+        fetch(
+            `https://movie-myflix-c346f5fde8cf.herokuapp.com/users/${user._id}/${movie._id}`,
+            { method: 'POST', headers: { Authorization: `Bearer ${token}` } }
+        )
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    console.log('Failed to add fav movie');
+                }
+            })
+            .then((user) => {
+                if (user) {
+                    //alert('successfully added to favorites');
+                    localStorage.setItem('user', JSON.stringify(user));
+                    setUser(user);
+                    setIsFavorite(true);
+                }
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    };
+
+    //Use fetch() to remove movie from fav-movie-list
+    const removeFavoriteMovie = () => {
+        fetch(
+            `https://movie-myflix-c346f5fde8cf.herokuapp.com/users/${user._id}/${movie._id}`,
+            { method: 'DELETE', headers: { Authorization: `Bearer ${token}` } }
+        )
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    alert('Failed');
+                }
+            })
+            .then((user) => {
+                if (user) {
+                    //alert('successfully deleted from favorites');
+                    localStorage.setItem('user', JSON.stringify(user));
+                    setUser(user);
+                    setIsFavorite(false);
+                }
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    };
 
     return (
         <Card className="h-100 movie-card--bg">
@@ -39,11 +88,44 @@ export const MovieCard = ({ user, movie }) => {
                 >
                     <Card.Title>{movie.title}</Card.Title>
                     <div>
-                        {/* Use ternary operator to check if the movie is one of the favourite movies, if yes, then return "red-heart". */}
+                        {/* Use ternary operator to check if the movie is one of the favourite movies, if yes, then return "full-red-heart". */}
                         {isFavorite ? (
-                            <i className="fa-solid fa-heart fav-movie--clicked"></i>
+                            <Button
+                                className="btn-fav-movie"
+                                variant="link"
+                                onClick={removeFavoriteMovie}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="32"
+                                    height="32"
+                                    fill="red"
+                                    className="bi bi-heart-fill"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                                    />
+                                </svg>
+                            </Button>
                         ) : (
-                            <i className="fa-solid fa-heart fav-movie--btn"></i>
+                            <Button
+                                className="btn-fav-movie"
+                                variant="link"
+                                onClick={addFavoriteMovie}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="32"
+                                    height="32"
+                                    fill="red"
+                                    className="bi bi-heart"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                                </svg>
+                            </Button>
                         )}
                     </div>
                 </div>

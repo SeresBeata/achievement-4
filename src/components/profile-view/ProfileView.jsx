62b61c6c9d8092f0bec4da9dd96482c8862c45df
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-19 20:57:52 +0100

    Update ProfileView for btn and styling
    Import profile-view.scss and <Link> component. Integrate the class selectors from .scss file. Update the style of components. Add <Link> to navigate to "/users/update/:userId" in case of btn click.

--- src/components/profile-view/ProfileView.jsx
+++ src/components/profile-view/ProfileView.jsx
@@ -1,5 +1,9 @@
 //Import components from React Bootstrap
-import { Card, ListGroup, Row, Col } from 'react-bootstrap';
+import { Card, ListGroup, Row, Col, Button } from 'react-bootstrap';
+//import scss
+import './profile-view.scss';
+//import Link
+import { Link } from 'react-router-dom';
 
 //Create ProfileView child component and export it
 export const ProfileView = ({ user }) => {
@@ -12,51 +16,44 @@ export const ProfileView = ({ user }) => {
                 xs={12}
                 className="justify-content-md-center justify-content-sm-center"
             >
-                <Card
-                    style={{ textAlign: 'center', margin: '30px 20px 0 20px' }}
-                >
-                    <Card.Header
-                        style={{
-                            backgroundColor: '#322b2b',
-                            color: '#ede0df',
-                            fontWeight: 'bolder',
-                        }}
-                    >
-                        Your Profile
-                    </Card.Header>
+                <Card className="card card-border">
                     <ListGroup>
-                        <ListGroup.Item>
-                            <span
-                                style={{
-                                    color: '#2c2425',
-                                    fontWeight: 'bolder',
-                                }}
-                            >
+                        <ListGroup.Item className="card-border listgroup-heading">
+                            <h3 className="card-heading">YOUR PROFILE</h3>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item className="card-border listgroup-item">
+                            <span className="listgroup-item--info">
                                 Username:
                             </span>{' '}
-                            {user.username}
+                            <span style={{ color: '#de4545' }}>
+                                {user.username}
+                            </span>
                         </ListGroup.Item>
-                        <ListGroup.Item>
-                            <span
-                                style={{
-                                    color: '#2c2425',
-                                    fontWeight: 'bolder',
-                                }}
-                            >
-                                Email:
-                            </span>{' '}
-                            {user.email}
+                        <ListGroup.Item className="card-border listgroup-item">
+                            <span className="listgroup-item--info">Email:</span>{' '}
+                            <span style={{ color: '#de4545' }}>
+                                {user.email}
+                            </span>
                         </ListGroup.Item>
-                        <ListGroup.Item>
-                            <span
-                                style={{
-                                    color: '#2c2425',
-                                    fontWeight: 'bolder',
-                                }}
-                            >
+                        <ListGroup.Item className="card-border listgroup-item">
+                            <span className="listgroup-item--info">
                                 Birthday:
                             </span>{' '}
-                            {user.birthday.split('T')[0]}
+                            <span style={{ color: '#de4545' }}>
+                                {user.birthday.split('T')[0]}
+                            </span>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="card-border listgroup-item">
+                            <Link
+                                to={`/users/update/${encodeURIComponent(
+                                    user._id
+                                )}`}
+                            >
+                                <Button className="card-button">
+                                    To update
+                                </Button>
+                            </Link>
                         </ListGroup.Item>
                     </ListGroup>
                 </Card>
